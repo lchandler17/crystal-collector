b29e3a0f447f7cd400ed4e10e6d3bd746840d95c
Fig Chandler <Fig@Laurens-MacBook-Air.local>
2017-05-16 00:01:06 -0400

    working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,72 +12,69 @@ var sofarnum = 0;
 
 var win = 0;
 var lose = 0;
+var gameOver = true;
 
-//generate a random number and display
-targetnum = Math.floor((Math.random() * 101) + 19);
-$(".random").text(targetnum);
-$(".count").text(sofarnum);
-$(".win").text(win);
-$(".lose").text(lose);
+function winLose() {
+	//if sofarnum > targetnum, increase lose count and reset other variables
+	if (sofarnum > targetnum) {
+		$(".lose").text(++lose);
+		gameOver = true;
+		alert("You lose! Try again?");
+	}
+	//if sofarnum = targetnum, increase win count and reset other variables
+	if (sofarnum === targetnum) {
+		$(".win").text(++win);
+		gameOver = true;
+		alert("You won! Play again?");
+	}
+}
 
+	//set &/or reset
+function reset() { 
+	if (gameOver === true) {
+		gameOver = false;
+		$(".win").text(win);
+		$(".lose").text(lose);
+		sofarnum = 0;
+		$(".count").text(sofarnum);
+		targetnum = Math.floor((Math.random() * 101) + 19);
+		$(".random").text(targetnum);
+		bluegem = Math.floor((Math.random() * 12) + 1);
+		greengem = Math.floor((Math.random() * 12) + 1);
+		purplegem = Math.floor((Math.random() * 12) + 1);
+		pinkgem = Math.floor((Math.random() * 12) + 1);
+		console.log(bluegem, greengem, purplegem, pinkgem);
+	}
+}
 
-//generate random values between __ and __ for each gem
-bluegem = Math.floor((Math.random() * 12) + 1);
-greengem = Math.floor((Math.random() * 12) + 1);
-purplegem = Math.floor((Math.random() * 12) + 1);
-pinkgem = Math.floor((Math.random() * 12) + 1);
+//START THE GAME
+reset();
 
-//check values for testing
-console.log(bluegem, greengem, purplegem, pinkgem);
-
-//when a gem is clicked, add that value to the sofarnum and re-write to page
+//when a gem is clicked, add that value to the sofarnum, re-write to page, and check the score for win/lose
 $("#bluegem").on("click", function() {
 	sofarnum = parseInt(bluegem + sofarnum);
-	$(".count").text(sofarnum)
+	$(".count").text(sofarnum);
+	winLose();
+	reset();
 });
 $("#greengem").on("click", function() {
 	sofarnum = parseInt(greengem + sofarnum);
-	$(".count").text(sofarnum)
+	$(".count").text(sofarnum);
+	winLose();
+	reset();
 });
 $("#purplegem").on("click", function() {
 	sofarnum = parseInt(purplegem + sofarnum);
-	$(".count").text(sofarnum)
+	$(".count").text(sofarnum);
+	winLose();
+	reset();
 });
 $("#pinkgem").on("click", function() {
 	sofarnum = parseInt(pinkgem + sofarnum);
-	$(".count").text(sofarnum)
+	$(".count").text(sofarnum);
+	winLose();
+	reset();
 });
 
-//if sofarnum > targetnum, increase lose count and reset other variables
-if (sofarnum > targetnum) {
-	alert("You lose! Try again?");
-	$(".lose").text(lose++);
-	//reset
-	targetnum = Math.floor((Math.random() * 101) + 19);
-	$(".random").text(targetnum);
-	sofarnum = 0;
-	$(".count").text(sofarnum);
-	bluegem = Math.floor((Math.random() * 12) + 1);
-	greengem = Math.floor((Math.random() * 12) + 1);
-	purplegem = Math.floor((Math.random() * 12) + 1);
-	pinkgem = Math.floor((Math.random() * 12) + 1);
-	console.log(bluegem, greengem, purplegem, pinkgem);
-}
-
-//if sofarnum = targetnum, increase win count and reset other variables
-if (sofarnum === targetnum) {
-	alert("You won! Play again?");
-	$(".win").text(win++);
-	//reset
-	targetnum = Math.floor((Math.random() * 101) + 19);
-	$(".random").text(targetnum);
-	sofarnum = 0;
-	$(".count").text(sofarnum);
-	bluegem = Math.floor((Math.random() * 12) + 1);
-	greengem = Math.floor((Math.random() * 12) + 1);
-	purplegem = Math.floor((Math.random() * 12) + 1);
-	pinkgem = Math.floor((Math.random() * 12) + 1);
-	console.log(bluegem, greengem, purplegem, pinkgem);
-}
 
 });
